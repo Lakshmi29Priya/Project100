da188a50fc08c342d91f9d08a36c7a968f2de59a
Lakshmi29Priya <73296967+Lakshmi29Priya@users.noreply.github.com>
2021-05-22 17:54:47 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,16 @@
+class Students:
+    def __init__(student,name,rollNo,marks):
+        student.name=name
+        student.rollNo=rollNo
+        student.marks=marks
+
+    def stun(student):
+        print("sam")
+    def stur(student):
+        print(2)
+    def stum(student):
+        print(8)
+
+sam = Students("tom",2,80)  
+
+sam.stun()              
